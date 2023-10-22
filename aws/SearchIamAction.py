59599326b047761d89ef52f0c5c232a24e3cea08
corutopi <tekitou@tekitou>
2023-10-22 16:12:40 +0000

    ログ出力追加

--- aws/SearchIamAction.py
+++ aws/SearchIamAction.py
@@ -4,6 +4,10 @@ description:
 
 requirements:
     boto3
+    tqdm
+
+todo:
+    ログ改善: funcNameが常にmy_loggingになってしまう件
 """
 
 import sys
@@ -11,11 +15,33 @@ import argparse
 import json
 import pprint
 import re
+import logging
+import tqdm
 
 import boto3
 
 client = boto3.client('iam')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s : %(levelname)s : %(module)s : %(funcName)s : %(message)s'
+    # stream=sys.stdout
+    )
+log_flg = True
 # target_action = 'tag:GetResources'
+
+def my_logging(message: str):
+    """
+    ログ出力する.
+    """
+    if not log_flg: return
+    logging.log(logging.INFO, message)
+
+def my_progress_bar(tar: list):
+    """
+    listでforループさせるとプログレスバーが表示されるようにする.
+    """
+    return tqdm.tqdm(tar) if log_flg else tar
+    
 
 def listnize(obj) -> list:
     """
@@ -36,13 +62,15 @@ def hasAction(document: dict, action: str) -> bool:
                 return True
     return False
 
-def main(target_action):
+def main(target_action: str):
+    re = dict()
     # IAM Role
     target_roles = []
     roles = client.list_roles()
     roles = [r['RoleName'] for r in roles['Roles']]
-    # pprint.pprint(roles)
-    for role in roles:
+    
+    my_logging('check iam roles start.')
+    for role in my_progress_bar(roles):
         # インラインポリシー
         role_policies = client.list_role_policies(RoleName=role)['PolicyNames']
         
@@ -72,21 +100,17 @@ def main(target_action):
             if hasAction(policy_document, target_action):
                 target_roles.append(role)
                 break
-        
-    print('============================')
-    pprint.pprint(target_roles)
+    my_logging('check iam roles end.')
     
     # todo: IAM User
     target_users = []
     users = [u['UserName'] for u in client.list_users()['Users']]
-    pprint.pprint(users)
     
-    pprint.pprint(client.get_user_policy(UserName='ss-chkOnepanman-gitaction-user', PolicyName='ecr_access'))
-    
-    for user in users:
+    my_logging('check iam users start.')
+    for user in my_progress_bar(users):
         # インラインポリシー
         user_policies = client.list_user_policies(UserName=user)['PolicyNames']
-        pprint.pprint(user_policies)
+        # pprint.pprint(user_policies)
         for user_policy in user_policies:
             policy = client.get_user_policy(UserName=user, PolicyName=user_policy)
             if hasAction(policy['PolicyDocument'], target_action):
@@ -96,7 +120,7 @@ def main(target_action):
     
         # アタッチポリシー
         attached_policies = client.list_attached_user_policies(UserName=user)
-        pprint.pprint(attached_policies)
+        # pprint.pprint(attached_policies)
         
         for attached_policy in attached_policies['AttachedPolicies']:
             policy = client.get_policy(PolicyArn=attached_policy['PolicyArn'])
@@ -112,11 +136,11 @@ def main(target_action):
         
         # 所属グループポリシー
         groups = [g['GroupName'] for g in client.list_groups_for_user(UserName=user)['Groups']]
-        print(f'groups={groups}')
+        # print(f'groups={groups}')
         for group in groups:
             # インラインポリシー
             group_policies = client.list_group_policies(GroupName=group)
-            pprint.pprint(group_policies)
+            # pprint.pprint(group_policies)
             for group_policy in group_policies['PolicyNames']:
                 policy = client.get_group_policy(GroupName=group, PolicyName=group_policy)
                 if hasAction(policy['PolicyDocument'], target_action):
@@ -126,7 +150,7 @@ def main(target_action):
             
             # アタッチポリシー
             attached_policies = client.list_attached_group_policies(GroupName=group)
-            pprint.pprint(attached_policies)
+            # pprint.pprint(attached_policies)
             
             for attached_policy in attached_policies['AttachedPolicies']:
                 policy = client.get_policy(PolicyArn=attached_policy['PolicyArn'])
@@ -139,10 +163,11 @@ def main(target_action):
                 if hasAction(policy_document, target_action):
                     target_users.append(user)
                     break
-            
-        
-    pprint.pprint(target_users)
-    
+    my_logging('check iam users end.')
+
+    re['roles'] = target_roles
+    re['users'] = target_users
+    return re
     # todo: IAM Policy
 
 if __name__ == '__main__':
@@ -152,9 +177,11 @@ if __name__ == '__main__':
         description='list iam role and user with the privileges specified in arg',
         )
     parser.add_argument('target_action')
-    parser.add_argument('-l', '--logging', action='store_true', help='outputs the processing progress as a log.')
+    parser.add_argument('-l', '--logging', action='store', default='True', choices=['True', 'False'], help='when True (default), outputs the processing progress as a log.')
 
     args = vars(parser.parse_args())
     # target_action = args.get('target_action', 'tag:GetResources')
+    log_flg = True if args['logging'] != 'False' else False
     
-    main(args['target_action'])
+    pprint.pprint(main(args['target_action']))
+    
