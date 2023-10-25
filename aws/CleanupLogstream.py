1ecb4db0552021a3a2c2f79ee226185145e9b508
corutopi <water_resist10ber@yahoo.co.jp>
2023-10-25 16:15:37 +0000

    プロトタイプ作成

--- aws/CleanupLogstream.py
+++ aws/CleanupLogstream.py
@@ -6,4 +6,21 @@ requirements:
     boto3
 """
 
-# todo: すべて
+import pprint
+
+import boto3
+
+client = boto3.client('logs', region_name='ap-northeast-1')
+
+dlg = client.describe_log_groups(limit=10)
+pprint.pprint(dlg)
+log_groups = [l['logGroupName'] for l in dlg['logGroups']]
+
+for log_group in log_groups:
+    log_streams = client.describe_log_streams(logGroupName=log_group,limit=10)['logStreams']
+    for log_stream in log_streams:
+        if log_stream['storedBytes'] == 0:
+            client.delete_log_stream(logGroupName=log_group, logStreamName=log_stream['logStreamName'])
+            print(f'delete log stream (LogGroup: {log_group}, LogStream:{log_stream["logStreamName"]})')
+    break
+
