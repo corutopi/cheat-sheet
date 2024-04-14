4920301da56d304b0fd4a3c22665893c99080b99
corutopi <water_resist10ber@yahoo.co.jp>
2024-04-14 17:02:22 +0000

    IMDSv2 に対応

--- aws/default-userdata.sh
+++ aws/default-userdata.sh
@@ -8,10 +8,22 @@ yum -y install jq
 
 
 # インスタンス名をタグ名に変更
+## https://docs.aws.amazon.com/ja_jp/AWSEC2/latest/UserGuide/instance-identity-documents.html
+## IMDSv1
+# hostnamectl set-hostname $(aws ec2 describe-instances \
+#     --query 'Reservations[].Instances[].Tags[?Key==`Name`].Value' \
+#     --filter "Name=instance-id,Values=`curl -s 'http://169.254.169.254/latest/meta-data/instance-id'`" \
+#     --region `curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'` \
+#     --output text)
+
+# IMDSv2
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+REGION=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
+INSTANCEID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .instanceId`
 hostnamectl set-hostname $(aws ec2 describe-instances \
     --query 'Reservations[].Instances[].Tags[?Key==`Name`].Value' \
-    --filter "Name=instance-id,Values=`curl -s 'http://169.254.169.254/latest/meta-data/instance-id'`" \
-    --region `curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'` \
+    --filter "Name=instance-id,Values=${INSTANCEID}" \
+    --region ${REGION} \
     --output text)
 
 
